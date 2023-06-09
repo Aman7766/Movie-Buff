2cc73b30c5cb04f63361948916b567a1a54d58f5
Amandeep <amankumar283@gmail.com>
2023-06-09 09:24:05 -0400

    Done

--- app/src/main/java/com/amandeep/moviebuff/LoginActivity.java
+++ app/src/main/java/com/amandeep/moviebuff/LoginActivity.java
@@ -3,6 +3,7 @@ package com.amandeep.moviebuff;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -10,15 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     TextView signup_text;
+    Button login;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         signup_text=findViewById(R.id.ls_bottom);
+        login=findViewById(R.id.login);
         signup_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this, MainPage.class);
                 startActivity(intent);
             }
         });
